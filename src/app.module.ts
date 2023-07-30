e2a347fd754a4d13b24b39c17a54fa8d81840073
Vincy-Cheng <60846680+Vincy-Cheng@users.noreply.github.com>
2023-07-30 19:18:59 +0800

    Set up database(MySQL) connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,40 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { datasource } from './datasource';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
     }),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // return {
+        //   type: 'mysql',
+        //   host: configService.get('DB_HOST'),
+        //   port: configService.get('DB_PORT')
+        //     ? parseInt(configService.get('DB_PORT'))
+        //     : 3306,
+        //   username: configService.get('DB_USERNAME'),
+        //   password: configService.get('DB_PASSWORD'),
+        //   database: configService.get('DB_DATABASE'),
+        //   entities: ['dist/**/entities/*.entity.{ts,js}'],
+        //   migrations: ['dist/migrations/*.{ts,js}'],
+        //   autoLoadEntities: true,
+        //   synchronize: false,
+        // };
+
+        return { ...datasource.options } as TypeOrmModuleOptions;
+      },
+    }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
