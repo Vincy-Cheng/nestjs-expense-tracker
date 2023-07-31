f6afb879c78c652704e6f11b0159d919e64648e6
Vincy-fivestones <vincy.c@fivestones.net>
2023-07-31 17:04:07 +0800

    add update password dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,8 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
+import { PickType } from '@nestjs/swagger';
+import { User } from '../entities/user.entity';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PickType(User, [
+  'id',
+  'username',
+  'email',
+]) {}
