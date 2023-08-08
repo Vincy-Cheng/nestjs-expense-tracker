7f0b11ea55dee3ba1420cc05c8d65964fe3b6df7
Vincy-fivestones <vincy.c@fivestones.net>
2023-08-08 12:52:28 +0800

    add bearer auth in swagger

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Expense Tracker')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
