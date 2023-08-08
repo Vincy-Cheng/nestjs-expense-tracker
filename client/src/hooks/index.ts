8d3a4dfdace613c63c5a8125d9fc19e3d49d70f9
Vincy-fivestones <vincy.c@fivestones.net>
2023-08-08 17:23:45 +0800

    update hook's index.ts

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -2,9 +2,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import type { TypedUseSelectorHook } from 'react-redux';
 import type { RootState, AppDispatch } from '../store/index';
 import { useDarkMode } from './useDarkMode';
+import { useOutsideAlerter } from './useOutsideAlerter';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-export { useDarkMode };
+export { useDarkMode, useOutsideAlerter };
