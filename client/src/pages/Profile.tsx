7f6e6ca987a546e233fcf27a4f40db72e055b867
Vincy-fivestones <vincy.c@fivestones.net>
2023-08-16 11:59:24 +0800

    Add 2 new pages

---
+++ client/src/pages/Profile.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BackButton from '../components/BackButton';
+
+type Props = {};
+
+const Profile = (props: Props) => {
+  return (
+    <div>
+      <BackButton />
+    </div>
+  );
+};
+
+export default Profile;
---
+++ client/src/pages/UpdatePassword.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BackButton from '../components/BackButton';
+
+type Props = {};
+
+const UpdatePassword = (props: Props) => {
+  return (
+    <div>
+      <BackButton />
+    </div>
+  );
+};
+
+export default UpdatePassword;
