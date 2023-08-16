a07b2b5eff597c7838183d9767d0b3465bbbb9eb
Vincy-fivestones <vincy.c@fivestones.net>
2023-08-16 11:55:59 +0800

    Install dnd-kit and toastify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,15 +4,33 @@ import { router } from './routes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import {
+  DndContext,
+  MouseSensor,
+  closestCenter,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
 
 export const queryClient = new QueryClient();
 
 function App() {
+  const sensors = useSensors(
+    useSensor(MouseSensor, {
+      // Require the mouse to move by 10 pixels before activating
+      activationConstraint: {
+        distance: 10,
+      },
+    }),
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
+      <DndContext collisionDetection={closestCenter} sensors={sensors}>
+        <Provider store={store}>
+          <RouterProvider router={router} />
+        </Provider>
+      </DndContext>
     </QueryClientProvider>
   );
 }
